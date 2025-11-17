3820ee886b2fe3468f5348d695318f6973595f22
MathiasTL <mathias.torres@unmsm.edu.pe>
2025-11-16 23:29:02 -0500

    Add structure of back and front

---
+++ backend-sqrc/src/main/java/com/sqrc/module/backendsqrc/BackendSqrcApplication.java
@@ -0,0 +1,13 @@
+package com.sqrc.module.backendsqrc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendSqrcApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BackendSqrcApplication.class, args);
+    }
+
+}
---
+++ backend-sqrc/src/test/java/com/sqrc/module/backendsqrc/BackendSqrcApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sqrc.module.backendsqrc;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendSqrcApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
