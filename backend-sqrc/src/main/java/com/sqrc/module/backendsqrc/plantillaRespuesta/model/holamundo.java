df40d152f81831ad45ab38c11d7d443103f9af8a
“Giancarlo <correo@gmail.comgit config --global user.mail Gianca_0603@outlook.com>
2025-11-20 12:50:06 -0500

    prueba

---
+++ backend-sqrc/src/main/java/com/sqrc/module/backendsqrc/plantillaRespuesta/model/holamundo.java
@@ -0,0 +1,7 @@
+package com.sqrc.module.backendsqrc.plantillaRespuesta.model;
+
+public class holamundo {
+    public static void main(String[] args) {
+        System.out.println("hola mundo");
+    }
+}
