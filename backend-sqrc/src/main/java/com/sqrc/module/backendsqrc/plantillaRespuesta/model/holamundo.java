accf4a08b1a4c499e24abcd75ba5087640856907
GiancarloVD <giancarlo.villavicencio@unmsm.edu.pe>
2025-11-20 13:04:23 -0500

    prueba2

--- backend-sqrc/src/main/java/com/sqrc/module/backendsqrc/plantillaRespuesta/model/holamundo.java
+++ backend-sqrc/src/main/java/com/sqrc/module/backendsqrc/plantillaRespuesta/model/holamundo.java
@@ -3,5 +3,6 @@ package com.sqrc.module.backendsqrc.plantillaRespuesta.model;
 public class holamundo {
     public static void main(String[] args) {
         System.out.println("hola mundo");
+        System.out.println("adios mundo");
     }
 }
