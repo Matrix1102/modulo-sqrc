eebf11d539cc1d9a973847a6d77a64b082fcf4ea
GiancarloVD <giancarlo.villavicencio@unmsm.edu.pe>
2025-11-24 01:04:31 -0500

    pequeños combios

--- backend-sqrc/src/main/java/com/sqrc/module/backendsqrc/plantillaRespuesta/DTO/PlantillaResponseDTO.java
+++ backend-sqrc/src/main/java/com/sqrc/module/backendsqrc/plantillaRespuesta/DTO/PlantillaResponseDTO.java
@@ -2,12 +2,16 @@ package com.sqrc.module.backendsqrc.plantillaRespuesta.DTO;
 
 import com.sqrc.module.backendsqrc.plantillaRespuesta.model.TipoCaso;
 
+import java.time.LocalDateTime;
+
 public record PlantillaResponseDTO(
+        Long idPlantilla,
         String nombreInterno,
         String tituloVisible,
         TipoCaso tipoCaso,
         boolean activo,
-        String fechaCreacion,
+        LocalDateTime fechaCreacion,
+        LocalDateTime fechaModificacion,
         String cuerpo,
         String despedida
 ) {
--- backend-sqrc/src/main/java/com/sqrc/module/backendsqrc/plantillaRespuesta/DTO/PreviewResponseDTO.java
+++ backend-sqrc/src/main/java/com/sqrc/module/backendsqrc/plantillaRespuesta/DTO/PreviewResponseDTO.java
@@ -1,7 +1,7 @@
 package com.sqrc.module.backendsqrc.plantillaRespuesta.DTO;
 
 public record PreviewResponseDTO(
-        Integer templateId,  //revisar luego
+        Long templateId,  //revisar luego
         String titulo,
         String htmlRenderizado
 ) {
